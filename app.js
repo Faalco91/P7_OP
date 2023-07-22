7c5b57475b13d8675713939ce1edb4f5dae5385f
Ouail Amarir <ouailamarir@gmail.com>
2023-07-22 13:49:48 +0200

    mise en place du server mongodb

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Faalco:Maghrib212@cluster0.cukera3.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('requete reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({message: 'Votre requête a bien été reçue lol.'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('La réponse a été envoyé avec succès !');
+app.use('/api/stuff' ,(req, res, next) => {
+    console.log('req.body');
+    res.status(201).json({
+        message: 'Objet crée !'
+    });
 });
 
 //On exporte cette constante app afin de pouvoir l'utiliser depuis nos autres fichiers notamment dans notre serveur node (server.js)
